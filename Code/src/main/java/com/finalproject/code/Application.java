16b5f06015ecea260d39cc815ac91b192f5b3195
JuliaKolano <juliakolano02@gmail.com>
2025-01-18 14:04:39 +0000

    created the login page layout

--- Code/src/main/java/com/finalproject/code/Application.java
+++ Code/src/main/java/com/finalproject/code/Application.java
@@ -10,9 +10,9 @@ import java.util.Objects;
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("user-library-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1000, 600);
-        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("home.css")).toExternalForm());
+        scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("login.css")).toExternalForm());
         stage.setTitle("Book Library");
         stage.setScene(scene);
         stage.show();
