77b9f730d3218781238aaebc1ccb1f1b4644fa5a
JuliaKolano <juliakolano02@gmail.com>
2025-02-04 17:55:43 +0000

    created the layout for the reading goal page

--- Code/src/main/java/com/finalproject/code/ReadingGoalController.java
+++ Code/src/main/java/com/finalproject/code/ReadingGoalController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -15,5 +16,15 @@ import java.io.IOException;
 import java.util.Objects;
 
 public class ReadingGoalController {
+    @FXML
+    private ToggleButton readingGoalToggle;
 
+    @FXML
+    private void onReadingGoalToggled() {
+        if (readingGoalToggle.isSelected()) {
+            readingGoalToggle.setText("Yes");
+        } else {
+            readingGoalToggle.setText("No");
+        }
+    }
 }
